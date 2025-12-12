c805c98588f753d7888961984f22d8b55e5b45d2
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-12-12 22:06:15 +0530

    Time: 0 ms (100%), Space: 8.2 MB (15.91%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -3,7 +3,7 @@ public:
     int trailingZeroes(int n) {
         int cnt = 0;
         while(n > 0){
-            cnt += n/5;
+            cnt += n / 5;
             n /= 5;
         }
         return cnt;
