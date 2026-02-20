1c761634cb2f9e455e4c467cb7b25fa735dcac4d
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-02-20 21:57:50 +0530

    Time: 0 ms (100%), Space: 14 MB (77.58%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin() ,  nums1.end());
-        sort(nums2.begin() ,  nums2.end());
-        int i = 0 , j = 0 ;
+        //step 1 :_ sort both array
+        sort(nums1.begin() , nums1.end());
+        sort(nums2.begin() , nums2.end());
         vector<int>ans;
-        while( i < nums1.size() && j < nums2.size()){
-            if(nums1[i] == nums2[j]   ){
-                if(ans.empty() || ans.back() != nums1[i]){
-                    ans.push_back(nums1[i]);
-                }
+        //step 2 : set pointer
+        int i = 0 , j = 0;
+        while(i < nums1.size() && j < nums2.size()){
+            //case1  
+            if(nums1[i] == nums2[j]){
+                ans.push_back(nums1[i]);
                 i++;
                 j++;
-                
-            }else if(nums1[i] < nums2[j]){
-                i++;
-            }else{
-                j++;
-            }
+
+            }else if(nums1[i] > nums2[j]) j++;
+            else i++;
         }
-        return ans;
+        //step 3 :- handle dublicate
+        vector<int>result;
+        if(!ans.empty())result.push_back(ans[0]);
+        for(int i = 1 ;i < ans.size() ; i++){
+            if(ans[i] == ans[i-1])continue;
+            else result.push_back(ans[i]);
+        }
+        return result;
     }
 };
