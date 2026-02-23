f1c24c2aebe006c047414791bef1d62e523e34e4
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-02-23 23:23:58 +0530

    Time: 17 ms (72.72%), Space: 45.6 MB (66.91%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,33 +1,22 @@
-import java.util.*;
-
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int n = nums.length;
-        if (n == 3)
-            return nums[0] + nums[1] + nums[2];
-        Arrays.sort(nums);
-        int closestSum = nums[0] + nums[1] + nums[2];
-        for (int i = 0; i < n - 2; i++) {
-            int left = i + 1;
-            int right = n - 1;
-            while (left < right) {
+         int n = nums.length;
+         Arrays.sort(nums);
+        int closestsum = nums[0] + nums[1] + nums[2];
+        if(n == 3)return closestsum;
+        for(int i = 0 ; i  < n - 2 ; i++){
+            int left = i+1 ;
+            int right = n-1;
+            while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
-                // update closest sum
-                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
-                    closestSum = sum;
+                if(Math.abs(sum - target) < Math.abs( closestsum - target)){
+                    closestsum = sum;
                 }
-                if (sum < target) {
-                    left++;
-                } 
-                else if (sum > target) {
-                    right--;
-                } 
-                else {
-                    return target; // exact match
-                }
+                if(sum == target )return target;
+                else if(sum < target)left++;
+                else right--;
             }
         }
-
-        return closestSum;
+        return closestsum;
     }
 }
