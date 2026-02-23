54acce18fb765d3e34c25d694392b9a422198b1e
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-02-23 23:51:50 +0530

    Time: 8 ms (90.66%), Space: 49.5 MB (12.71%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,29 +1,17 @@
-import java.util.*;
-
 class Solution {
     public int[][] merge(int[][] intervals) {
-
-        // sort intervals based on start time
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
+        Arrays.sort(intervals, (a , b)-> Integer.compare(a[0] , b[0]));
         List<int[]> ans = new ArrayList<>();
-
-        // add first interval
+        int n = intervals.length;
         ans.add(intervals[0]);
-
-        for (int i = 1; i < intervals.length; i++) {
-
-            int[] lastInterval = ans.get(ans.size() - 1);
-
-            // overlapping condition
-            if (intervals[i][0] <= lastInterval[1]) {
-                lastInterval[1] = Math.max(lastInterval[1], intervals[i][1]);
-            } else {
+        for(int i = 1 ; i < n ; i++){
+           int[] lastinterval = ans.get(ans.size() -1);
+            if(intervals[i][0] <= lastinterval[1]){
+                ans.get(ans.size() - 1)[1] = Math.max(lastinterval[1] , intervals[i][1]);
+            }else{
                 ans.add(intervals[i]);
             }
         }
-
-        // convert List<int[]> → int[][]
         return ans.toArray(new int[ans.size()][]);
     }
 }
