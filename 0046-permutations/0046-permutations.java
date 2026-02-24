8a9b9e2ebc4f89c09e3ad27c61253544e5e78d58
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-02-24 21:37:05 +0530

    Time: 0 ms (100%), Space: 45.3 MB (90.6%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,42 +1,29 @@
-import java.util.*;
-
 class Solution {
+    public void solve(int[] nums , List<List<Integer>> ans , int index){
 
-    public void solve(int[] nums, List<List<Integer>> ans, int index) {
-
-        // base case
-        if (index >= nums.length) {
+        //base case
+        if(index >= nums.length){
             List<Integer> temp = new ArrayList<>();
-            for (int num : nums) {
-                temp.add(num);
-            }
+            for(int num : nums)temp.add(num);
             ans.add(temp);
+
             return;
         }
-
-        for (int i = index; i < nums.length; i++) {
-
-            // swap
-            swap(nums, index, i);
-
-            // recursive call
-            solve(nums, ans, index + 1);
-
-            // backtrack (swap back)
-            swap(nums, index, i);
+        for(int i = index ; i < nums.length ; i++){
+           swap(nums , i , index);
+           solve(nums , ans , index + 1 );
+            swap(nums , i , index);
         }
     }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    private void swap(int[] arr , int start , int end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
     }
 
     public List<List<Integer>> permute(int[] nums) {
-
         List<List<Integer>> ans = new ArrayList<>();
-        solve(nums, ans, 0);
+        solve(nums , ans , 0);
         return ans;
     }
 }
