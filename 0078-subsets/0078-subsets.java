d4e49899cac3807a1fd7c244d0aa494a488a67a3
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-02-26 23:06:44 +0530

    Time: 0 ms (100%), Space: 44.4 MB (19.83%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,30 +1,20 @@
-import java.util.*;
-
 class Solution {
-
-    private void solve(int[] nums, List<Integer> output, int index, List<List<Integer>> ans) {
-        // Base case
-        if (index >= nums.length) {
-            ans.add(new ArrayList<>(output)); // add copy of current subset
+    public void solve(int[] nums , List<Integer>output , List<List<Integer>> ans , int index){
+        if(index >= nums.length){
+            ans.add(new ArrayList<>(output));
             return;
         }
-
-        // Exclude current element
-        solve(nums, output, index + 1, ans);
-
-        // Include current element
+        //exclude
+        solve(nums , output , ans , index + 1 );
+        //include
         output.add(nums[index]);
-        solve(nums, output, index + 1, ans);
-
-        // Backtrack (remove last element)
+        solve(nums , output , ans  , index + 1);
         output.remove(output.size() - 1);
     }
-
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> output = new ArrayList<>();
-
-        solve(nums, output, 0, ans);
+        solve(nums , output , ans , 0);
         return ans;
     }
 }
