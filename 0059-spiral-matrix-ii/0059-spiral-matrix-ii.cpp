e464ddc92a81435132cb52fefe5a7658aae802fd
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-03-02 22:54:32 +0530

    Time: 0 ms (100%), Space: 9.5 MB (6%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>>ans(n , vector<int>(n , 0));
-        int top = 0 , left = 0 , right = n-1 , bottom = n-1;
-        int num = 1;
-        while(num <= n * n){
-            //left to right
+        vector<vector<int>> ans(n , vector<int>(n));
+        int top = 0 , bottom = n-1 , left = 0 , right = n-1 , index = 1;
+        while(top <= bottom && left <= right){
             for(int i = left ; i <= right ; i++){
-                 ans[top][i] = num++;
+                ans[top][i] = index++ ;
+
             }
             top++;
-            
-            //top to bottom
             for(int i = top ; i <= bottom ; i++){
-                ans[i][right] = num++;
+                ans[i][right] = index++;
+
             }
             right--;
-            
-            //right to left 
             if(top <= bottom){
-                for(int i = right ;i >= left ; i--){
-                    ans[bottom][i] = num++;
+                for(int i = right ; i >= left ; i--){
+                    ans[bottom][i] = index++;
+
                 }
                 bottom--;
             }
-            //bottom to top
             if(left <= right){
                 for(int i = bottom ; i >= top ; i--){
-                    ans[i][left] = num++;
+                    ans[i][left] = index++;
+
                 }
                 left++;
             }
         }
         return ans;
-
     }
 };
